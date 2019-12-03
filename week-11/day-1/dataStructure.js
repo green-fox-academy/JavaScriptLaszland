4506b07304244a206810fe19f25b3a0151d50173
Laci <laszlo.roland.kiss@gmail.com>
2019-12-03 21:32:31 +0100

    finish todo

--- week-11/day-1/dataStructure.js
+++ week-11/day-1/dataStructure.js
@@ -1,10 +1,6 @@
 'use strict';
 
 let example = 'In a dishwasher far far away';
-
-// I would like to replace "dishwasher" with "galaxy" in this example
-// Please fix it for me!
-// Expected ouput: In a galaxy far far away
 console.log(example.replace('dishwasher', 'galaxy'));
 
 function reverse(text) {
@@ -14,3 +10,25 @@ function reverse(text) {
 let reversed = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
 console.log(reverse(reversed));
 console.log(reversed);
+
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indention
+
+// Expected outpt:
+
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
+
+
+String.prototype.splice = function(start, delCount, newSubStr) {
+    return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
+};
+let todoText = ' - Buy milk\n';
+todoText = 'My todo:\n' + todoText;
+todoText += ' - Download games\n';
+todoText += ' \t- Diabolo';
+
+console.log(todoText);
