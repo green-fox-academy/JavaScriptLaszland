76b9483f27390ae50750867846733ba7e2b8a947
Laci <laszlo.roland.kiss@gmail.com>
2019-12-03 16:22:31 +0100

    start and finish bubble sort

---
+++ week-11/bubble.js
@@ -0,0 +1,19 @@
+let listOfNumber = [12, 54, 67, 3, 23, 6, -1];
+
+let bubbleSort = (inputArr) => {
+    let len = inputArr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < len; i++) {
+            if (inputArr[i] > inputArr[i + 1]) {
+                let tmp = inputArr[i];
+                inputArr[i] = inputArr[i + 1];
+                inputArr[i + 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return inputArr;
+};
+console.log(bubbleSort(listOfNumber));
