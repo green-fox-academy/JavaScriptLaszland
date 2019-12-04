d53cc39332d2f17c62205d7ccbcafde59eed38fc
Laci <laszlo.roland.kiss@gmail.com>
2019-12-04 16:13:34 +0100

    finish content-1

---
+++ week-11/day-3/content-1.js
@@ -0,0 +1,4 @@
+var selectedElements = document.getElementsByTagName('p');
+selectedElements[0].innerHTML = selectedElements[3].innerHTML;
+selectedElements[1].innerHTML = selectedElements[3].innerHTML;
+selectedElements[2].innerHTML = selectedElements[3].innerHTML;
