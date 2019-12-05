2bd2e2bae9c2beffc2b91544ae6e34c12b6af114
Laci <laszlo.roland.kiss@gmail.com>
2019-12-05 14:43:18 +0100

    finish svg

---
+++ week-11/day-4/svg-1.js
@@ -0,0 +1,4 @@
+let selectedRect = document.getElementById('very-rectangle');
+selectedRect.setAttribute('x', '50');
+selectedRect.setAttribute('y', '50');
+selectedRect.setAttribute('style', 'stroke:#ff0000; stroke-width: 10; fill: tomato');
